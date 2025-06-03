7ad9cb86d3e8768bb79f73b23029d5203d59106b
DESIGN-SG <tsuruta89npb@yahoo.co.jp>
2025-06-03 16:24:12 +0900

    add HeadContent

---
+++ src/components/head/HeadContent /index.jsx
@@ -0,0 +1,3 @@
+export default function HeadContent() {
+  return <p>こんにちは！</p>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as MyFirstReact } from './MyFirstReact';
 export { default as MySecondReact } from './MySecondReact';
 export { default as MyThirdReact } from './MyThirdReact';
 export { default as MyFourthReact } from './MyFourthReact';
+export { default as HeadContent } from './head/HeadContent';
