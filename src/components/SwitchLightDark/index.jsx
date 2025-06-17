6a46a7f99336244e2fd3d24e7c14ca7eafd8b0f1
DESIGN-SG <tsuruta89npb@yahoo.co.jp>
2025-06-17 12:35:40 +0900

    関数定義後にexport default

--- src/components/SwitchLightDark/index.jsx
+++ src/components/SwitchLightDark/index.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import styles from './SwitchLightDark.module.scss';
 
-export default function SwitchLightDark() {
+function SwitchLightDark() {
   const [colorMode, setColor] = useState(false);
 
   useEffect(() => {
-    document.body.style.backgroundColor = colorMode ? "#444" : "#fff";
+    document.body.style.backgroundColor = colorMode ? "#444" : "#f7f7f7";
     document.body.style.transition = "background .3s";
   }, [colorMode]);
 
@@ -19,3 +19,5 @@ export default function SwitchLightDark() {
     </>
   );
 }
+
+export default SwitchLightDark;
