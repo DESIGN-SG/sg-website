1e04dd1bd8d774b7010a8b981d3709d4c4290331
DESIGN-SG <tsuruta89npb@yahoo.co.jp>
2025-07-06 17:00:51 +0900

    SidebarSns, SnsIcon update

--- src/components/Icon/SnsIcon/index.jsx
+++ src/components/Icon/SnsIcon/index.jsx
@@ -1,9 +1,9 @@
 import styles from './SnsIcon.module.scss';
 
-function SnsIcon({ linkUrl, iconImage, imgAlt }) {
+function SnsIcon({ linkUrl, iconImage, imgAlt, anchorId }) {
   return (
     <a href={linkUrl} target="_blank" className={styles.IconWrap}>
-      <span>
+      <span style={{ anchorName: anchorId }}>
         <img src={iconImage} alt={imgAlt} />
       </span>
     </a>
--- src/components/Sidebar/SidebarSns/index.jsx
+++ src/components/Sidebar/SidebarSns/index.jsx
@@ -4,9 +4,30 @@ import SnsIcon from '../../Icon/SnsIcon'
 function SidebarSns() {
   return (
     <ul className={styles.SnsWrapper}>
-      <li><SnsIcon linkUrl="https://x.com/DESIGN_SG_" iconImage="/icon/sns/sns_x.svg" imgAlt="X" /></li>
-      <li><SnsIcon linkUrl="https://zenn.dev/design_sg" iconImage="/icon/sns/sns_zenn.svg" imgAlt="Zenn" /></li>
-      <li><SnsIcon linkUrl="https://github.com/DESIGN-SG" iconImage="/icon/sns/sns_github.svg" imgAlt="GitHub" /></li>
+      <li>
+        <SnsIcon 
+          linkUrl="https://x.com/DESIGN_SG_"
+          iconImage="/icon/sns/sns_x.svg" 
+          imgAlt="X"
+          anchorId="--item-1"
+        />
+      </li>
+      <li>
+        <SnsIcon
+          linkUrl="https://zenn.dev/design_sg"
+          iconImage="/icon/sns/sns_zenn.svg"
+          imgAlt="Zenn"
+          anchorId="--item-2"
+        />
+      </li>
+      <li>
+        <SnsIcon
+          linkUrl="https://github.com/DESIGN-SG"
+          iconImage="/icon/sns/sns_github.svg"
+          imgAlt="GitHub"
+          anchorId="--item-3"
+        />
+      </li>
     </ul>
   );
 }
