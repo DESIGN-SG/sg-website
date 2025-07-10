0f50635a12622d190823971b0d5c04ab1afcabb0
DESIGN-SG <tsuruta89npb@yahoo.co.jp>
2025-07-10 23:43:21 +0900

    画像の読み込み速度改善

--- src/components/Icon/SnsIcon/index.jsx
+++ src/components/Icon/SnsIcon/index.jsx
@@ -4,7 +4,7 @@ function SnsIcon({ linkUrl, iconImage, imgAlt, anchorId }) {
   return (
     <a href={linkUrl} target="_blank" className={styles.IconWrap}>
       <span style={{ anchorName: anchorId }}>
-        <img src={iconImage} alt={imgAlt} />
+        <img src={iconImage} alt={imgAlt} loading="eager" />
       </span>
     </a>
   );
--- src/components/head/link/LinkTags/index.jsx
+++ src/components/head/link/LinkTags/index.jsx
@@ -2,6 +2,11 @@ export default function LinkTags() {
   return (
     <>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link rel="preload" href="/logo/logo_white.png" as="image" />
+      <link rel="preload" href="/logo/logo.png" as="image" />
+      <link rel="preload" href="/icon/sns/sns_x.svg" as="image" />
+      <link rel="preload" href="/icon/sns/sns_zenn.svg" as="image" />
+      <link rel="preload" href="/icon/sns/sns_github.svg" as="image" />
     </>
   );
 }
