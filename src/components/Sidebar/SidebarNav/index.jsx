e2e1af9485a9c95722ae6a4c10f35cb53946a294
DESIGN-SG <tsuruta89npb@yahoo.co.jp>
2025-07-26 01:06:47 +0900

    update SidebarNav

--- src/components/Sidebar/SidebarNav/index.jsx
+++ src/components/Sidebar/SidebarNav/index.jsx
@@ -3,11 +3,11 @@ import styles from "./SidebarNav.module.scss"
 function SidebarNav() {
   return (
     <ul className={styles.SidebarNav}>
-      <li><a href="/about">コンテンツ</a></li>
-      <li><a href="">コンテンツ</a></li>
-      <li><a href="">コンテンツ</a></li>
-      <li><a href="">コンテンツ</a></li>
-      <li><a href="">コンテンツ</a></li>
+      <li><a href="/about">コンテンツ<span></span></a></li>
+      <li><a href="">コンテンツ<span></span></a></li>
+      <li><a href="">コンテンツ<span></span></a></li>
+      <li><a href="">コンテンツ<span></span></a></li>
+      <li><a href="">コンテンツ<span></span></a></li>
     </ul>
   );
 }
